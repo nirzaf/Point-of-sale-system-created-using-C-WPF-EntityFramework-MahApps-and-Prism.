f78693b0dd4e8ca9071d51e00a779846947ebdc0
jayeuperio <jayeuperio@yahoo.com>
2017-08-24 12:53:42 +0800

    ADD: Test Data

---
+++ TestDb/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PoS.Dal.Mdl;
+using PoS.Dal.Sql.Ctx;
+using PoS.Dal.Sql.Ctx.Repository;
+
+namespace TestDb
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			PoSDbTransact dbTransact = new PoSDbTransact("PosDbConn");
+
+			var users = dbTransact.UnitOfWork.UserRepo.GetAll ().ToList ();
+		}
+	}
+}
