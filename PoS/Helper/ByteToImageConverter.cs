a8725218475b5d0a71094cd7b85a22e3e8455e2c
jayeuperio <jayeuperio@yahoo.com>
2017-09-01 08:37:21 +0800

    Change the Image converter

--- PoS/Helper/ByteToImageConverter.cs
+++ PoS/Helper/ByteToImageConverter.cs
@@ -1,37 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace PoS.Helper
 {
-	[ValueConversion(typeof(byte[]), typeof(ImageSource))]
-	public class ByteToImageConverter : IValueConverter
+	[ValueConversion(typeof(BitmapImage), typeof(byte[]))]
+	public class ByteToImageConverter: IValueConverter
 	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
 		{
 			if (value is byte[]) {
-				byte[] data = value as byte[];
-				BitmapImage im = new BitmapImage();
+				byte[] data = (byte[])value;
 				MemoryStream stream = new MemoryStream(data);
-				im.BeginInit();
-				im.StreamSource = stream;
-				im.EndInit();
+				BitmapImage bitImage = new BitmapImage();
 
-				return im;
+				bitImage.BeginInit ();
+				bitImage.StreamSource = stream;
+				bitImage.EndInit ();
+				return bitImage;
 			}
-
 			return null;
 		}
 
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
 		{
+			if (value is BitmapImage) {
+				BitmapImage imSrc = value as BitmapImage;
+				Stream str = imSrc.StreamSource;
+				byte[] oBuffer = null;
+
+				if (str != null && str.Length != 0) {
+					using (BinaryReader br = new BinaryReader (str)) {
+						oBuffer = br.ReadBytes ((int)str.Length);
+					}
+
+					return oBuffer;
+				}
+			}
+
 			return null;
 		}
 	}
